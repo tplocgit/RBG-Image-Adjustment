3322d823c4503cb10d5d0b5a58b66e0d4720eacf
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2020-08-18 22:40:56 +0700

    ADD: Class Editor + Method (brightness + contrast + menu)

--- source.py
+++ source.py
@@ -2,69 +2,108 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+
 NUM_CHANNELS = 3
 IMG_EXTENSIONS = ('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')
 MAX_BRIGHTNESS = 255
-STUFFS = ["Brightness", "Contrast", "RGB to Grayscale", "Flip", "Stacking", "Blurring"]
+BRIGHTNESS = 0
+CONTRAST = 1
+GRAYSCALE = 2
+FLIP = 3
+STACKING = 4
+BLURRING = 5
+CHANGE_IMAGE = 6
+EXIT = 7
+STUFFS = ["Brightness", "Contrast", "Grayscale", "Flip", "Stacking", "Blurring", "Change image", "Exit"]
 
 
+def is_image(entry):
+    return entry.is_file() and entry.name.lower().endswith(IMG_EXTENSIONS) and entry.name
 
-def get_img_list():
+
+class MyImageEditor:
     """
-    make a list of input items from folder INPUT
-    :param directory:
-    :return: list of input directory items as list of string
+    Contains functions that adjust image
     """
-    img_list = []
-    with os.scandir() as i:
-        for entry in i:
-            if entry.is_file():
-                if entry.name.lower().endswith(IMG_EXTENSIONS):
-                    img_list.append(entry.name)
 
-    return img_list
+    def __init__(self, img_lst=None, tar=None, stuffs=None):
+        self.images = img_lst if img_lst else []
+        self.target = tar
+        self.can_do = stuffs
 
+    def update_images(self) -> None:
+        """
+        Store all image's name in current folder
+        """
+        with os.scandir() as scanner:
+            self.images = [entry.name for entry in scanner if is_image(entry)]
 
-def menu(stuffs=None):
-    stuffs = STUFFS if not stuffs else []
-    menu_stuffs = dict(zip([i for i in range(len(stuffs))], stuffs))
-    print("Index\t:\tStuff")
-    for item in menu_stuffs.items():
-        print("{}\t\t:\t{}".format(item[0], item[1]))
+    def set_target(self, index=None):
+        """
+        Set target to adjusting
+        :param index: Index of target in list of images
+        """
+        with Image.open(self.images[index]) as img:
+            self.target = np.array(img)
 
+    def show_target(self):
+        """
+        Show current image
+        """
+        plt.imshow(self.target)
+        plt.show()
 
-def adjust_brightness(img_mat: np.array, factor: int, adjustment="Increase"):
-    if adjustment == "Increase":
-        img_mat[img_mat + factor <= MAX_BRIGHTNESS] += factor
-    else:
-        img_mat[img_mat - factor >=0] -= factor
-    return img_mat
+    def list_images(self):
+        print("Index\t|\tImage")
+        for i in range(len(self.images)):
+            print("\t{}\t|\t{}".format(i, self.images[i]))
 
+    def show_menu(self) -> None:
+        """
+        Print stuffs that program can do like Index | Stuff.
+        :return: None.
+        """
+        menu_stuffs = dict(zip([i for i in range(len(self.can_do))], self.can_do)) if self.can_do else []
+        if menu_stuffs:
+            print("Index\t:\tStuff")
+            for item in menu_stuffs.items():
+                print("{}\t\t:\t{}".format(item[0], item[1]))
+        else:
+            print("MAINTENANCES")
 
-def create_input(img, num_channels=NUM_CHANNELS):
-    """
-    Reshape 2d of vectors to 1d of vectors
-    :param img:
-    :return:
-    """
-    img_mat = np.array(img)
-    input_mat = img_mat.reshape(img_mat.shape[0] * img_mat.shape[1], num_channels)
-    return input_mat
+    def adjust_contrast_brightness(self, brightness_factor=0, contrast_factor=1.0) -> None:
+        """
+        Adjusts brightness of image.
+        :param contrast_factor:
+        :param brightness_factor: Factor by which image increase or decrease.
+        :param brightness_factor:
+        """
+        for i in range(self.target.shape[0]):
+            for j in range(self.target.shape[1]):
+                for k in range(self.target.shape[2]):
+                    mul = int(self.target[i, j, k] * contrast_factor)
+                    s = self.target[i, j, k] + brightness_factor
+                    self.target[i, j, k] = mul if mul <= MAX_BRIGHTNESS else MAX_BRIGHTNESS
+                    self.target[i, j, k] = s if s <= MAX_BRIGHTNESS else MAX_BRIGHTNESS
+                    self.target[i, j, k] = s if s >= 0 else 0
 
 
 if __name__ == "__main__":
-    input_imgs = get_img_list()
-    imgs_dict = dict(zip([i for i in range(len(input_imgs))],input_imgs))
-    print(imgs_dict)
-    selected = int(input("Select index of file to adjust: "))
-    print("Selected: ", imgs_dict[selected])
-    menu()
-    factor = int(input("Enter factor that increase or decrease the brightness of image: "))
-    msg = "Increase" if factor > 0 else "Decrease"
-    new_factor = abs(factor)
-    print(msg,"Brightness by", new_factor)
-    with Image.open(imgs_dict[selected]) as img:
-        mat = np.array(img)
-        mat = adjust_brightness(mat, new_factor,adjustment=msg)
-        plt.imshow(mat)
-        plt.show()
+    # Get list of image that can be adjusted
+    editor = MyImageEditor(stuffs=STUFFS)
+    editor.update_images()
+    editor.list_images()
+    selected = int(input("Select index of image in list that you want to adjust!\nYour choose: "))
+    while selected not in range(len(editor.images)):
+        selected = int(input("Invalid index please choose valid one in {}\nYour choose: "))
+    editor.set_target(index=selected)
+    selected = not EXIT
+    while selected != EXIT:
+        editor.show_menu()
+        selected = int(input("Your choose: "))
+        if selected == BRIGHTNESS:
+            factor = int(input("Input factor to adjust brightness must be in range [-100:100] for best: "))
+            editor.adjust_contrast_brightness(brightness_factor=factor)
+            editor.show_target()
+        else:
+            print("This feature not available yet, please choose another feature")
